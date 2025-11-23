e75d28b44946dea5038468a1e0166a25f20886bf
shijas <shijas@example.com>
2025-11-23 22:40:24 +0530

    Switch to Base Mainnet

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,5 +1,5 @@
 import { cookieStorage, createStorage } from 'wagmi'
-import { baseSepolia } from 'wagmi/chains'
+import { base } from 'wagmi/chains'
 import { AppKitNetwork } from '@reown/appkit/networks'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 
@@ -9,7 +9,7 @@ if (!projectId) {
     throw new Error('Project ID is not defined')
 }
 
-export const networks = [baseSepolia] as [AppKitNetwork, ...AppKitNetwork[]]
+export const networks = [base] as [AppKitNetwork, ...AppKitNetwork[]]
 
 export const wagmiAdapter = new WagmiAdapter({
     storage: createStorage({
